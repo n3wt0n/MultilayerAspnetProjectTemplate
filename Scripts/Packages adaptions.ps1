feeea4bcb21f76d58c3640f142cee841bb6ff41f
Davide Benvegnù <davide.benvegnu@outlook.com>
2016-04-21 11:04:53 +0800

    Added PS script

--- Scripts/Packages adaptions.ps1
+++ Scripts/Packages adaptions.ps1
@@ -1,13 +1,30 @@
-﻿Get-ChildItem -Recurse -Include "*.csproj" | Select-String "MVCMultiLayer" | Select-Object -Unique Path | foreach {
+﻿# Susbtitution for the references in .csproj files
+Get-ChildItem -Recurse -Include "*.csproj" | Select-String "MVCMultiLayer" | Select-Object -Unique Path | foreach {
     
-    $a = (Get-Content $_.Path).Replace("MVCMultiLayer", "$safeprojectname$")
-    $a | Out-File $_.Path -Force 
+    $a = (Get-Content $_.Path).Replace("MVCMultiLayer", '$safeprojectname$')
+    $a | Out-File $_.Path -Encoding utf8 -Force 
 } 
   
-
+# Packages folder change in .csproj files: when it create the solution, the "Packages" folder is one level down
 Get-ChildItem -Recurse -Include "*.csproj" | Select-String "packages" | Select-Object -Unique Path | foreach {
     
     $a = (Get-Content $_.Path).Replace("..\packages\", "..\..\packages\")
-    $a | Out-File $_.Path -Force 
+    $a | Out-File $_.Path -Encoding utf8 -Force 
+}
+
+# Change remaining fixed project names to dynamic
+Get-ChildItem -Recurse -Exclude ("*.zip","*InputStream*","*.vstemplate") | Select-String "MVCMultiLayer" | Select-Object -Unique Path | foreach {
+    if ($_ -ne "InputStream") {
+        $a = (Get-Content $_.Path).Replace("MVCMultiLayer", '$rootsafeprojectname$')
+        $a | Out-File $_.Path -Encoding utf8 -Force 
+    }
 } 
-  
+Get-ChildItem -Recurse -Exclude ("*.zip","*InputStream*","*.vstemplate") | Select-String 'rootsafeprojectname' | Select-Object -Unique Path | foreach {
+    if ($_ -ne "InputStream") {
+        $a = (Get-Content $_.Path)
+        $b = $a.Replace('$rootsafeprojectname$Db', "MVCMultiLayerDb")
+        if ($a -ne $b) {
+            $b | Out-File $_.Path -Encoding utf8 -Force 
+        }
+    }
+} 
