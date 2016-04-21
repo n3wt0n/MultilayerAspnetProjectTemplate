d4dc1df3a4ac4e0dcdcd91cb6dff7c8d37007a24
Davide Benvegnù <davide.benvegnu@outlook.com>
2016-04-21 11:12:28 +0800

    Added the Wizard for sing rootprojectname parameter

---
+++ Sources/MVCMultiLayer.TemplateWizard/ChildWizard.cs
@@ -0,0 +1,39 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using System.Collections.Generic;
+
+namespace MVCMultiLayer.TemplateWizard
+{
+    // Child wizard is used by child project vstemplates
+
+    public class ChildWizard : IWizard
+    {
+        // Retrieve global replacement parameters
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            // Add custom parameters.
+            replacementsDictionary.Add("$saferootprojectname$", RootWizard.GlobalDictionary["$saferootprojectname$"]);
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+    }
+}
---
+++ Sources/MVCMultiLayer.TemplateWizard/RootWizard.cs
@@ -0,0 +1,48 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using System.Collections.Generic;
+
+// This assembly must be signed and either installed in the GAC
+// or included as Content in a VSIX project.
+// Then it is referenced from vstemplates.
+// Custom replacement can be used in projects and files.
+
+namespace MVCMultiLayer.TemplateWizard
+{
+    // Root wizard is used by root project vstemplate
+
+    public class RootWizard : IWizard
+    {
+        // Use to communicate $saferootprojectname$ to ChildWizard
+        public static Dictionary<string, string> GlobalDictionary = new Dictionary<string, string>();
+
+        // Add global replacement parameters
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            // Place "$saferootprojectname$ in the global dictionary.
+            // Copy from $safeprojectname$ passed in my root vstemplate
+            GlobalDictionary["$saferootprojectname$"] = replacementsDictionary["$safeprojectname$"];
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+    }
+}
