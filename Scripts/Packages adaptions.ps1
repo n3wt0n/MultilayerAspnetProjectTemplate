5a0f27415a3fce2ec994d4479b57d50a9f8ec78c
Davide Benvegnù <davide.benvegnu@outlook.com>
2016-04-21 09:35:35 +0800

    Restored UTF-8 encoding on csproj files

---
+++ Scripts/Packages adaptions.ps1
@@ -0,0 +1,13 @@
+﻿Get-ChildItem -Recurse -Include "*.csproj" | Select-String "MVCMultiLayer" | Select-Object -Unique Path | foreach {
+    
+    $a = (Get-Content $_.Path).Replace("MVCMultiLayer", "$safeprojectname$")
+    $a | Out-File $_.Path -Force 
+} 
+  
+
+Get-ChildItem -Recurse -Include "*.csproj" | Select-String "packages" | Select-Object -Unique Path | foreach {
+    
+    $a = (Get-Content $_.Path).Replace("..\packages\", "..\..\packages\")
+    $a | Out-File $_.Path -Force 
+} 
+  
